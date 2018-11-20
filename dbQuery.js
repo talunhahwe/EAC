48e0de7b16d7249ba4611b9522dc9389145ecbe9
sukam09 <sukam09@naver.com>
2018-11-21 05:40:40 +0900

    test

---
+++ dbQuery.js
@@ -0,0 +1,5 @@
+var str='Hello, world!';
+
+function f(){
+    document.getElementById('test').innerHTML=str;
+}
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    res.end();
-}).listen(8080);
+var server = http.createServer(function(request,response){
+  var parsedUrl = url.parse(request.url);
+  var resource = parsedUrl.pathname;
+
+  if(resource == '/'){
+    fs.readFile('dbQuery.html', 'utf-8', function(error, data)
+    {
+      if(error){
+        response.writeHead(500, {'Content-Type':'text/html'});
+        response.end('500 Internal Server Error : '+error);
+      }else{
+          response.writeHead(200, {'Content-Type':'text/html'});
+          response.end(data);
+      }
+    });
+  }
+});
+
+server.listen(80, function(){
+  console.log('Server is running…');
+});
